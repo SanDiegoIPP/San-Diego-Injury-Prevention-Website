7fa8788e979081c6acdbb46c6a5491375f276f51
SanDiegoIPP <sdinjurypreventionprogram@gmail.com>
2026-09-14 23:01:56 -0700

    Change href links to relative paths in HomeImpactSection
    Updated href links to use relative paths instead of external URLs.

--- apps/website/src/components/ui/home-impact-section.tsx
+++ apps/website/src/components/ui/home-impact-section.tsx
@@ -11,35 +11,35 @@ const impactCards: ImpactPlanItem[] = [
     description:
       "Educate students in San Diego schools on the importance of injury prevention",
     icon: BookOpen,
-    href: "https://san-diego-injury-prevention-website.vercel.app/programs", // paste link here
+    href: "/programs", // paste link here
   },
   {
     title: "Health Fairs",
     description:
       "Provide helpful resources for local communities, promoting health and wellness",
     icon: Heart,
-    href: "https://san-diego-injury-prevention-website.vercel.app/programs", // paste link here
+    href: "/programs", // paste link here
   },
   {
     title: "Fall Prevention Classes",
     description:
       "Lead fitness, mobility, and tai chi classes for residents of nursing homes.",
     icon: Dumbbell,
-    href: "https://san-diego-injury-prevention-website.vercel.app/programs", // paste link here
+    href: "/programs", // paste link here
   },
   {
     title: "Community Outreach",
     description:
       "Promote health programs by distributing informational materials and engaging with local residents to increase awareness and participation.",
     icon: Megaphone,
-    href: "https://san-diego-injury-prevention-website.vercel.app/programs", // paste link here
+    href: "/programs", // paste link here
   },
   {
     title: "Collaboration",
     description:
       "Work with professionals from various hospitals and health groups to coordinate initiatives, share resources, and support efforts that improve public health outcomes.",
     icon: Handshake,
-    href: "https://san-diego-injury-prevention-website.vercel.app/more/partner-with-us", // paste link here
+    href: "/more/partner-with-us", // paste link here
   },
 ];
 
